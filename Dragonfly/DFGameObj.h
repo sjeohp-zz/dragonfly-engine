5c64cbd249b26895fdb06eb91c2c89b722601f02
Joseph Mark <sjeohp@gmail.com>
2014-01-22 15:26:21 +1300

    Adding some C++

---
+++ Dragonfly/DFGameObj.h
@@ -0,0 +1,14 @@
+//
+//  DFGameObj.h
+//  Dragonfly
+//
+//  Created by Joseph on 20/12/13.
+//  Copyright (c) 2013 Joseph Mark. All rights reserved.
+//
+
+#ifndef __Dragonfly__DFGameObj__
+#define __Dragonfly__DFGameObj__
+
+#include <iostream>
+
+#endif /* defined(__Dragonfly__DFGameObj__) */
---
+++ Dragonfly/DFGameObj.mm
@@ -0,0 +1,9 @@
+//
+//  DFGameObj.cpp
+//  Dragonfly
+//
+//  Created by Joseph on 20/12/13.
+//  Copyright (c) 2013 Joseph Mark. All rights reserved.
+//
+
+#include "DFGameObj.h"
